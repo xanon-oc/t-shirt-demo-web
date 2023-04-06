d4e36889a0fc79ce6de2f8873d3d81736584b242
NILOY ROY <121684663+xanon-oc@users.noreply.github.com>
2023-04-06 20:33:51 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./CSS/Header.css";
+const Header = () => {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/review">Review</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/components/OrderReview.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+
+const OrderReview = () => {
+  return (
+    <div>
+      <h2>Order Review Page</h2>
+    </div>
+  );
+};
+
+export default OrderReview;
